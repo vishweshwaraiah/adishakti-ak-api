2ef95e70d214aacc2a5fbf4f6477b76c90f7e2bd
Vishweshwarayya Kj <k.vishu@outlook.com>
2024-04-14 09:19:16 +0530

    AK-Api: code changes added

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
